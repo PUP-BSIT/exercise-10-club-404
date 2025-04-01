91fe16f449ab4baf65179561a3779a2fb296a3b5
arroyo-johnmatthew <johnmatthewarroyo2@gmail.com>
2025-04-01 13:03:51 +0800

    add sort comments by date function

--- arroyo_johnmatthew/script.js
+++ arroyo_johnmatthew/script.js
@@ -1,8 +1,27 @@
+const date = new Date().toLocaleString();
+const commentContent = [
+	{
+		user: "Justine",
+		date: "3/19/2025, 9:59:47 PM",
+		comment: "I hope you achieve your goals!",
+	},
+	{
+		user: "Patricia",
+		date: "3/19/2025, 10:09:26 PM",
+		comment: "𐙚⋆°｡⋆That's a great goal!",
+	},
+	{
+		user: "Bea",
+		date: "3/19/2025, 10:18:28 PM",
+		comment: "Achieve your goals, I know you can do it!",
+	}
+];
+
 function buttonEnabler() {
 	let name = document.getElementById("user_name");
 	let comment = document.getElementById("user_comment");
 
-	if(comment.value.length > 0 && name.value.length > 0) {
+	if(comment.value.length && name.value.length) {
 		document.getElementById("comment_button").disabled = false;
     } else {
 		document.getElementById("comment_button").disabled = true;
@@ -14,11 +33,20 @@ function displayComment() {
 	let commenter = document.getElementById("user_name");
 	let comment = document.getElementById("user_comment");
 	let button = document.getElementById("comment_button");
-	const newName = `<h2 class="commenter">${commenter.value}</h2>`;
-	const newComment = `<p class="comment-content">${comment.value}</p>`;
+	let commentInfo = document.getElementsByTagName("label");
+
+	const newComment = `<div class="comment-user">
+              				<h2 class="commenter">${commenter.value}</h2>
+              				<label class="date">${date}</label>
+              				<p class="comment-content">${comment.value}</p>
+            			</div>`;
 
 	if(!button.disabled) {
-		commentContainer.insertAdjacentHTML("beforeend", newName);
+		commentContent.push({
+			user: commenter.value,
+			date: date,
+			comment: comment.value
+		});
 		commentContainer.insertAdjacentHTML("beforeend", newComment);
 	}
 
@@ -26,3 +54,62 @@ function displayComment() {
 	comment.value = "";
 	button.disabled = true;
 }
+
+function sortAscending() {
+	let commentContainer = document.querySelectorAll(".comment-user");
+
+	commentContent.sort(function(a,b) {
+		let firstDate = new Date(a.date);
+		let secondDate = new Date(b.date);
+		
+		if (firstDate > secondDate) {
+			return 1;
+		}
+
+		if (firstDate < secondDate) {
+			return -1;
+		}
+
+		return 0;
+	})
+
+	for(let index = 0; index < commentContent.length; index++) {
+		commentContainer[index].children[0].innerHTML = 
+			commentContent[index].user;
+		commentContainer[index].children[1].innerHTML =
+			commentContent[index].date;
+		commentContainer[index].children[2].innerHTML =
+			commentContent[index].comment;
+	}
+	
+}
+
+function sortDescending() {
+	let commentContainer = document.querySelectorAll(".comment-user");
+
+	commentContent.sort(function(a,b) {
+		let firstDate = new Date(a.date);
+		let secondDate = new Date(b.date);
+		
+		if (firstDate < secondDate) {
+			return 1
+		}
+
+		if (firstDate > secondDate) {
+			return -1;
+		}
+
+		return 0;
+	})
+
+	for(let index = 0; index < commentContent.length; index++) {
+		commentContainer[index].children[0].innerHTML = 
+			commentContent[index].user;
+		commentContainer[index].children[1].innerHTML =
+			commentContent[index].date;
+		commentContainer[index].children[2].innerHTML =
+			commentContent[index].comment;
+	}
+}
+
+
